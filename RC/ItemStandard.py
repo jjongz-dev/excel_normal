c551c656da1cf4e11fb513df1e11f93bf7abe2b0
ckddnz <ckddnz@howbuild.com>
2022-07-12 12:18:01 +0900

    구조 엑셀 저장

--- RC/ItemStandard.py
+++ RC/ItemStandard.py
@@ -12,4 +12,4 @@ class ItemStandard:
     sum: float
 
     def to_excel(self):
-        return [self.floor, self.ho, '', '건축', '철근콘크리트공사', '', self.name, self.standard, '', self.part, '구조',self.formula, self.sum, '']
+        return [self.floor, self.location, '', '건축', '철근콘크리트공사', '', self.name, self.standard, '', self.part, '구조',self.formula, self.sum, '']
