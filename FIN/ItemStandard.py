115083596d62214a44436165d875cc0f7e97775f
ckddnz <ckddnz@howbuild.com>
2022-08-02 14:14:50 +0900

    마감 엑셀 저장

--- FIN/ItemStandard.py
+++ FIN/ItemStandard.py
@@ -13,11 +13,11 @@ class ItemStandard:
     name: str
     standard: str
     unit: str
-    part: str
+
     type: str
     formula: str
     sum: float
 
 
     def to_excel(self):
-        return [self.floor, self.location, self.roomname, '건축', '', '', self.name, self.standard, self.unit, self.part, self.type, self.formula, self.sum, '']
+        return [self.floor, self.location, self.roomname, '건축', '', '', self.name, self.standard, self.unit, '', self.type, self.formula, self.sum, '']
