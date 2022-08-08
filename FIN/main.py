3d6ce9803ebb6c515e75bb3426f59c40ba52d6ee
ckddnz <ckddnz@howbuild.com>
2022-08-08 19:16:56 +0900

    마감 엑셀 저장

--- FIN/main.py
+++ FIN/main.py
@@ -6,6 +6,8 @@ from FIN.ItemStandard import ItemStandard
 
 from FIN.ItemStandard2 import ItemStandard2
 
+import FloorRule
+
 
 def excel_normalize(name, column_dimensions=None):
     excel = load_workbook(
@@ -153,12 +155,8 @@ def excel_normalize(name, column_dimensions=None):
             item.roomname = '기초하부'
             item.floor = 'FT'
 
-        #기본 층정리
-        if (item.name in ['가설컨테이너반입', '가설컨테이너반출', '가설수도', '가설전기', '가설울타리설치', '가설울타리해체', '가설출입구설치', '가설출입구해체',
-                          '건축폐기물처리', '건축허가표지판', '경계측량및현황측량', '민원처리', '이동식가설화장실반입', '이동식가설화장실반출', '준공청소', '지내력시험', '규준틀설치'] ):
-            item.location = '공통가설'
-            item.roomname = '공통가설'
-            item.floor = '1F'
+        #기본 층정리 - TODO: Test!
+        FloorRule.launch(item)
 
         #조경공사
         if item.floor.__contains__('조경'):
