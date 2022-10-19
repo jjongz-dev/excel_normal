a2fc51dd097bd063f6c9fdc69b9b1a79b342becb
ckddnz <ckddnz@howbuild.com>
2022-10-19 18:52:11 +0900

    TEST

---
+++ Architect/CkddnzTest/simple.py
@@ -0,0 +1,11 @@
+
+
+languages = ['python', 'perl', 'c', 'java']
+
+for lang in languages:
+    if lang in ['python', 'perl']:
+        print("%6s need interpreter" % lang)
+    elif lang in ['c', 'java']:
+        print("%6s need compiler" % lang)
+    else:
+        print("shouild not reach here")
