eeb70597bb0cf006a7221d69c5881749ee3b71b3
jjongz-dev <jjongz@howbuild.com>
2022-12-15 12:02:13 +0900

    영등포동2가 마감 적산

---
+++ Architect/FIN/yeongdeungpo-dong2_finish.py
@@ -0,0 +1,186 @@
+import os
+
+import win32com.client
+from openpyxl import load_workbook, Workbook
+
+from Architect.FIN.ItemStandard import ItemStandard
+
+import re
+
+floor_regex = r"([PH]*[\d~]*[bB]*\d+F)_(.*)"
+window_regex = r"([A-Z_0-9]+)+\s/\s(\d+)\*(\d+).*\(면적:(\d+.\d+)\)"
+
+desktop_location = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+
+def excel_normalize(name, column_dimensions=None):
+    print(desktop_location)
+    excel = load_workbook(
+        desktop_location + '\\2.영등포동2가 다세대주택 및 오피스텔 신축공사(산출서)-11.15.xlsx',
+        # desktop_location + '\\건축.xlsx',
+        read_only=True,
+        data_only=True)
+
+    items = []
+
+    # index init
+    position_index = 0
+    name_index = 1
+    standard_index = 2
+    unit_index = 3
+    formular_index = 5
+    each_quantity_index = 6
+    count_index = 7
+    quantity_index = 8
+
+    # 각종 변수들 초기화
+    floor = location = room_name = type = ''
+    sheet_name = '1. 본관동-물량산출서'
+
+    if sheet_name in excel.sheetnames:
+        worksheet = excel[sheet_name]
+
+        for row in worksheet.iter_rows(min_col=0, max_col=worksheet._current_row, min_row=2):
+
+            if (row[position_index].value is not None
+                    and (row[name_index].value is None or row[name_index].value == '')
+                    and (row[standard_index].value is None or row[standard_index].value == '')
+                    and (row[unit_index].value is None or row[unit_index].value == '')
+                    and (row[formular_index].value is None or row[formular_index].value == '')
+                    and (row[each_quantity_index].value is None or row[each_quantity_index].value == '')
+                    and (row[count_index].value is None or row[count_index].value == '')
+                    and (row[quantity_index].value is None or row[quantity_index].value == '')
+            ):
+
+                # ASD_01 / 2000*2140 방화유리자동문포함(면적:4.28)
+                if is_window(row[position_index].value):
+                    name, width, height, area = window_name_stand(row[position_index].value)
+                    item = ItemStandard(
+                        floor=floor,
+                        location=location,
+                        roomname=room_name,
+                        name=name,
+                        standard=f'{format(int(width) / 1000.0, ".3f")}*{format(int(height) / 1000.0, ".3f")}={format(float(area), ".3f")}',
+                        unit="EA",
+                        type=type,
+                        formula=1,
+                        sum=1,
+                    )
+                    items.append(item)
+                    continue
+
+                # 공사명 : 영등포동2가 다세대주택 및 오피스텔 신축공사 > 본관동 > 창호
+                if ('>' in row[position_index].value and row[position_index].value != ''):
+                    value_split = row[position_index].value.split('>')
+                    type = value_split[-1].strip()
+                    continue
+
+                # B1F_기계실, FSD_02    [문]   0.95*2.1       (  1EA)
+                if is_floor(row[position_index].value):
+                    try:
+                        floor, location = floor_name(row[position_index].value)
+                        room_name = location
+                    except:
+                        print(f'{floor=}, {location=}')
+                    continue
+
+                # 정면도
+                # [가로]X    0.95  [세로]Y     2.1  [면적]A   1.995 ==> pass
+                if ('[' not in row[position_index].value and row[position_index].value != ''):
+                    try:
+                        floor = ''
+                        location = row[position_index].value
+                        room_name = row[position_index].value
+                    except:
+                        print(f'{value_split=}')
+                    continue
+
+            # 품명없음 삭제
+            if (row[name_index].value is None
+                    or row[name_index].value == ""
+            ):
+                continue
+
+            # 위치	품명	규격	단위	부위	산출식	물량	개소	수량	비고
+            if (row[position_index].value == '위치'
+                    and row[name_index].value == '품명'
+                    and row[standard_index].value == '규격'
+                    and row[unit_index].value == '단위'
+            ):
+                continue
+
+            if row[count_index].value is not None and int(str(row[count_index].value)) > 1:
+                formula = f'{row[formular_index].value} * {row[count_index].value}'
+            else:
+                formula = row[formular_index].value
+
+            item = ItemStandard(
+                floor=floor,
+                location=location,
+                roomname=room_name,
+                name=row[name_index].value,
+                standard=row[standard_index].value,
+                unit=row[unit_index].value,
+                type=type,
+                formula=formula,
+                sum=row[quantity_index].value,
+            )
+            items.append(item)
+
+    # 저장할 엑셀
+    new_workbook = Workbook()
+    new_sheet = new_workbook.active
+    new_sheet.title = '건축(데이터변경X)'
+    head_title = ['층', '호', '실', '대공종', '중공종', '코드', '품명', '규격', '단위', '부위', '타입', '산식', '수량', 'Remark']
+    new_sheet.append(head_title)
+    new_sheet.column_dimensions["G"].width = 30
+    new_sheet.column_dimensions["H"].width = 30
+    new_sheet.column_dimensions["L"].width = 30
+    try:
+        for item in items:
+            new_sheet.append(item.to_excel())
+    except:
+        print(item)
+
+    # 저장할 파일이 열려 있을 경우 창을 닫고 다시 저장!!
+    excel = win32com.client.Dispatch('Excel.Application')
+    try:
+        result_excel_file_name = desktop_location + "\\건축완성.xlsx"
+        new_workbook.save(result_excel_file_name)
+        excel.Workbooks.Open(result_excel_file_name)
+    except PermissionError as pe:
+        for workbook in excel.Workbooks:
+            full_name = workbook.FullName
+            if full_name == result_excel_file_name:
+                workbook.Close(True)
+                new_workbook.save(result_excel_file_name)
+                excel.Workbooks.Open(result_excel_file_name)
+
+
+def floor_name(text: str):
+    match = re.match(floor_regex, text)
+    return match.groups()
+
+
+def is_floor(text: str) -> bool:
+    match = re.match(floor_regex, text)
+    if match is None:
+        return False
+    else:
+        return match.lastindex == 2
+
+
+def window_name_stand(text: str):
+    match = re.match(window_regex, text)
+    return match.groups()
+
+
+def is_window(text: str) -> bool:
+    match = re.match(window_regex, text)
+    if match is None:
+        return False
+    else:
+        return match.lastindex == 4
+
+
+if __name__ == '__main__':
+    excel_normalize('PyCharm')
