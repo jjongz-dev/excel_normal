35edeb9867f675c1d2788fda2e54e599b420649b
hwangjinha <blue@howbuild.com>
2023-03-15 11:00:50 +0900

    .

--- Architect/FIN/건축_hjh_23-0199(논현동5-12)-창호도어공제면적삭제.py
+++ Architect/FIN/건축_hjh_23-0199(논현동5-12)-창호도어공제면적삭제.py
@@ -42,110 +42,85 @@ def excel_normalize(name, column_dimensions=None):
 
         for row in worksheet.iter_rows(min_row=2):
 
-            위치 = row[0].value
-            품명 = row[1].value
-            규격 = row[2].value
-            단위 = row[3].value
-            부위 = row[4].value
-            산식 = row[5].value
-            수량 = row[8].value
+            위치값 = row[0].value
+            품명값 = row[1].value
+            규격값 = row[2].value
+            단위값 = row[3].value
+            부위값 = row[4].value
+            산식값 = row[5].value
+            수량값 = row[8].value
 
 ####### 타입 / 층 / 실명 뽑아내기  위치에만 데이터가 있는 항목들 대상  S ##############################
 
-            if (위치 is not None
-                    and (품명 is None or 품명 == '')
-                    and (규격 is None or 규격 == '')
-                    and (단위 is None or 단위 == '')
-                    and (부위 is None or 부위 == '')
-                    and (산식 is None or 산식 == '')
-                    and (수량 is None or 수량 == '')
+            if (위치값 is not None
+                    and (품명값 is None or 품명값 == '')
+                    and (규격값 is None or 규격값 == '')
+                    and (단위값 is None or 단위값 == '')
+                    and (부위값 is None or 부위값 == '')
+                    and (산식값 is None or 산식값 == '')
+                    and (수량값 is None or 수량값 == '')
             ):
 
-                위치분리빈칸 = 위치.split(' ')
-                위치분리빈칸길이 = len(위치분리빈칸)
 
-                위치분리슬래쉬 = 위치.split('/')
-                위치분리슬래쉬길이 = len(위치분리슬래쉬)
-
-
-                #print(위치, '/', 위치분리빈칸[0], '/', 위치분리빈칸길이)
-
-                if '>' in 위치:
-                    타입확정 = 위치.split('>')[-1]
+                if '>' in 위치값:
+                    타입확정 = 위치값.split('>')[-1].strip()
                     continue
 
-                if "/" in 위치:
-                    층확정 = 위치분리슬래쉬[0].strip()
-                    호확정 = 위치분리슬래쉬[0].strip()
-                    실확정 = 위치분리슬래쉬[0].strip()
+                if "/" in 위치값:
 
-                    print(위치, '/', 위치분리슬래쉬[0], '/', 위치분리슬래쉬길이)
+                    위치값_분리_슬래쉬 = 위치값.split('/')
+                    창호명 = 위치값_분리_슬래쉬[0].strip()
+                    창호사이즈 = 위치값_분리_슬래쉬[1].split('(')[0]
+                    창호가로사이즈 = 창호사이즈.split('*')[0]
+                    창호세로사이즈 = 창호사이즈.split('*')[1]
+
+                    층확정 = 창호명
+                    호확정 = 창호명
+                    실확정 = 창호명
+
+#                    print(위치, '  ==> ', 위치값_분리_슬래쉬[0], '/', 위치값_분리_슬래쉬[1], ' ===>', 호확정 ,' ===>', 창호가로사이즈, "/", 창호세로사이즈)
+
                     continue
 
-                if '[문]' in 위치 or '[창]' in 위치:
-                    continue
+                위치값_분리_빈칸 = 위치값.split(' ')
+                위치값_분리_빈칸_길이 = len(위치값_분리_빈칸)
 
-                # B301 근린생활   -> 층과 실로 분리하여 내역에 적용
-                if 위치분리빈칸길이 == 1:
+                                # B301 근린생활, 전기실  등에서  층과 실 추출
+
+                if 위치값_분리_빈칸_길이 == 1:
                     층확정 = ''
-                    호확정 = 위치분리빈칸[0]
-                    실확정 = 위치분리빈칸[0]
-                    continue
-
-                elif 위치분리빈칸길이 == 2:
-                    층확정 = 위치분리빈칸[0]
-                    호확정 = 위치분리빈칸[1]
-                    실확정 = 위치분리빈칸[1]
-
-                   # print(층확정, 호확정, 실확정)
-                    continue
-                elif ('[가로]X' in 위치 or '[둘레]L' in 위치):
+                    호확정 = 위치값_분리_빈칸[0]
+                    실확정 = 위치값_분리_빈칸[0]
                     continue
                 else:
+                    층확정 = 위치값_분리_빈칸[0]
+                    호확정 = 위치값_분리_빈칸[1]
+                    실확정 = 위치값_분리_빈칸[1]
+                    continue
 
-                    품명 = 'XXXXX'
-                    규격 = '*****'
-                    수량 = 777
+                if '[문]' in 위치값 or '[창]' in 위치값:
+                    continue
 
-                    #if 위치분리빈칸길이 > 1:
+                if '[가로]X' in 위치값 or '[둘레]L' in 위치값:
+                    continue
 
-                        #for i, col in enumerate(위치분리빈칸):
-                          # print(i, '/', col)
-
-                        # print(층확정, 호확정, 실확정, 품명확정, 규격확정, 단위확정, 타입확정, 산식확정, 수량확정)
-
-####### 타입 / 층 / 실명 뽑아내기  위치에만 데이터가 있는 항목들 대상  E ##############################
-
-
-            if (위치 == '위치'
-                    and 품명 == '품명'
-                    and 규격 == '규격'
-            ):
+            if 위치값 == '위치' and 품명값 == '품명' and 규격값 == '규격':
                 continue
 
-
-            if ((위치 is None or 위치 == '')
-                    and (품명 is None or 품명 == '')
-                    and (규격 is None or 규격 == '')
-            ):
+            if (위치값 is None or 위치값 == '') and (품명값 is None or 품명값 == '') and (규격값 is None or 규격값 == ''):
                 continue
 
-            if (('[가로]X' in 위치 or '[둘레]L' in 위치 or '공사명' in 위치)
-                    and (품명 is None or 품명 == '')
-                    and (규격 is None or 규격 == '')):
+            if ('[가로]X' in 위치값 or '[둘레]L' in 위치값 or '공사명' in 위치값) and (품명값 is None or 품명값 == '') and (규격값 is None or 규격값 == ''):
                 continue
 
-            if (품명 is not None and 수량 is not None):
+            if 품명값 is not None and 수량값 is not None:
 
-                품명확정 = 품명
-                규격확정 = 규격
-                단위확정 = 단위
-                부위확정 = row[4].value
-                산식확정 = row[5].value
-                수량확정 = row[8].value
-
-                if 수량 is not None:
-                    수량확정 = 수량
+                품명확정 = 품명값
+                규격확정 = 규격값
+                단위확정 = 단위값
+                부위확정 = 부위값
+                산식확정 = 산식값
+                수량확정 = 수량값
 
                 내역 = ExcelStandard(
                     층=층확정,
@@ -175,8 +150,8 @@ def excel_normalize(name, column_dimensions=None):
     new_sheet.column_dimensions["H"].width = 15
 
     for 내역 in 내역목록:
-        new_sheet.append(내역.to_excel())
-        new_workbook.save(saveFilePath)
+       new_sheet.append(내역.to_excel())
+       new_workbook.save(saveFilePath)
 
 
 if __name__ == '__main__':
