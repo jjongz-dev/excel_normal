49482eae8bf1fe3014909804076332d51300af06
hwangjinha <blue@howbuild.com>
2023-03-27 13:36:52 +0900

    내부적산 - 건축

--- ExcelGroup.py
+++ ExcelGroup.py
@@ -11,7 +11,7 @@ class ExcelGroup:
 
     def to_excelGroup(self):
 
-        공종 = self.중공종
+        중공종 = self.중공종
         품명 = self.품명
         규격 = self.규격
         단위 = self.단위
@@ -26,4 +26,4 @@ class ExcelGroup:
         if type(단위) is str:
             단위 = 단위.replace('\n', '').replace('\r', '').replace('m', 'M').replace('m2', 'M2').replace('m3', 'M3').replace('ton', 'TON').replace('㎡', 'M2').replace('㎥', 'M3').replace('ea', 'EA')
 
-        return [공종, 품명, 규격, 단위, 수량]
+        return [중공종, 품명, 규격, 단위, 수량]
