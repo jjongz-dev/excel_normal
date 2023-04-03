4045e32efb0863bd1f7afdd0ff5168072862f599
hwangjinha <blue@howbuild.com>
2023-04-03 09:51:39 +0900

    토목 수정

--- Architect/RC/구조.py
+++ Architect/RC/구조.py
@@ -6,7 +6,7 @@ fileCreateDate = datetime.strftime(datetime.today(), '%Y%m%d_%H%M')
 
 
 # 이곳에 현장 폴더명만 변경하면 완료 #######
-siteTicketNo = '22-0068'
+siteTicketNo = '23-0233'
 ##################################
 
 openFilePath = '/Users/blue/hb/quantity/'+siteTicketNo+'/구조.xlsx'
