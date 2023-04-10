162b4a382bbc289dd78910ce191116d882d8959d
hwangjinha <blue@howbuild.com>
2023-04-10 09:48:28 +0900

    내부

--- Architect/FIN/건축.py
+++ Architect/FIN/건축.py
@@ -12,7 +12,7 @@ fileCreateDate = datetime.strftime(datetime.today(), '%Y%m%d_%H%M')
 
 
 # 이곳에 현장 폴더명만 변경하면 완료 #######
-siteTicketNo = '23-0183'
+siteTicketNo = '22-1178'
 ##################################
 
 openFilePath = '/Users/blue/hb/quantity/'+siteTicketNo+'/건축.xlsx'
